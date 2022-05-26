389643fbb5953b8a57c57b8f57547244252e482f
Grosgibier <53627391+Grosgibier@users.noreply.github.com>
2022-05-26 11:00:16 +0200

    login captcha

--- Calendrier/index2.php
+++ Calendrier/index2.php
@@ -5,7 +5,7 @@
 ?>
 <!DOCTYPE html>
 <html>
- <head>
+<head>
   <title>Réservation pour consultation</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
@@ -14,109 +14,111 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
   <script>
-   
-  $(document).ready(function() {
-   var calendar = $('#calendar').fullCalendar({
-    editable:true,
+
+      $(document).ready(function() {
+         var calendar = $('#calendar').fullCalendar({
+            editable:true,
     defaultView:'agendaWeek', // Mode d'affichage par défaut
-    height: 500, // Hauteur du calendrier
+    height: 300, // Hauteur du calendrier
     aspectRatio: 0.1,
 
     header:{
-     left:'prev,next today',
-     center:'title',
-     right:''
-    },
-    events: 'load.php',
-    selectable:true,
-    selectHelper:true,
+       left:'prev,next today',
+       center:'title',
+       right:''
+   },
+   events: 'load.php',
+   selectable:true,
+   selectHelper:true,
 
-    select: function(start, end, allDay)
-    {
-     var title = prompt("Enter Event Title");
-     if(title)
-     {
-      var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-      var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-      $.ajax({
-       url:"insert.php",
-       type:"POST",
-       data:{title:title, start:start, end:end},
-       success:function()
+   select: function(start, end, allDay)
+   {
+       var title = prompt("Enter Event Title");
+       if(title)
        {
-        calendar.fullCalendar('refetchEvents');
-        alert("Added Successfully");
-       }
-      })
-     }
-    },
-    editable:true,
-    eventResize:function(event)
-    {
-     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-     var title = event.title;
-     var id = event.id;
-     $.ajax({
+          var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+          var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+          $.ajax({
+             url:"insert.php",
+             type:"POST",
+             data:{title:title, start:start, end:end},
+             success:function()
+             {
+                calendar.fullCalendar('refetchEvents');
+                alert("Added Successfully");
+            }
+        })
+      }
+  },
+  editable:true,
+  eventResize:function(event)
+  {
+   var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+   var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+   var title = event.title;
+   var id = event.id;
+   $.ajax({
       url:"update.php",
       type:"POST",
       data:{title:title, start:start, end:end, id:id},
       success:function(){
-       calendar.fullCalendar('refetchEvents');
-       alert('Event Update');
-      }
-     })
-    },
+         calendar.fullCalendar('refetchEvents');
+         alert('Event Update');
+     }
+ })
+},
 
-    eventDrop:function(event)
-    {
-     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-     var title = event.title;
-     var id = event.id;
-     $.ajax({
+eventDrop:function(event)
+{
+   var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+   var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+   var title = event.title;
+   var id = event.id;
+   $.ajax({
       url:"update.php",
       type:"POST",
       data:{title:title, start:start, end:end, id:id},
       success:function()
       {
-       calendar.fullCalendar('refetchEvents');
-       alert("Event Updated");
-      }
-     });
-    },
+         calendar.fullCalendar('refetchEvents');
+         alert("Event Updated");
+     }
+ });
+},
 
-    eventClick:function(event)
-    {
-     if(confirm("Are you sure you want to remove it?"))
-     {
+eventClick:function(event)
+{
+   if(confirm("Are you sure you want to remove it?"))
+   {
       var id = event.id;
       $.ajax({
-       url:"delete.php",
-       type:"POST",
-       data:{id:id},
-       success:function()
-       {
-        calendar.fullCalendar('refetchEvents');
-        alert("Event Removed");
-       }
-      })
-     }
-    },
+         url:"delete.php",
+         type:"POST",
+         data:{id:id},
+         success:function()
+         {
+            calendar.fullCalendar('refetchEvents');
+            alert("Event Removed");
+        }
+    })
+  }
+},
 
-   });
-  });
-   
-  </script>
- </head>
- <body>
+});
+     });
+
+ </script>
+</head>
+<body>
   <br />
   <h2 align="center"><a href="#">Réservation pour consultation</a></h2>
+  <h2 align="right"><a href="save.php">Envoyer les disponibilités</a></h2>
   <br />
+
   <div class="container">
-   <div id="calendar"></div>
-  </div>
-  
- </body>
+     <div id="calendar"></div>
+     <a href="save.php"></a>
+ </div>
+</body>
 </html>
 
