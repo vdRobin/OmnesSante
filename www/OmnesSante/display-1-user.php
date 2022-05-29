5e69d2bf860a941af73f08172552ca5a95ee3ad8
Balthou <balthazar.augier@gmail.com>
2022-05-29 19:56:10 +0200

    affichage des CV

--- www/OmnesSante/display-1-user.php
+++ www/OmnesSante/display-1-user.php
@@ -14,16 +14,16 @@
         ?>
     </p>
     <!-- Button to launch a modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" style="background-color: rgb(143, 198, 248);border-color: rgb(143, 198, 248)"> Voir CV</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal<?= $row['utilisateurID'] ?>" style="background-color: rgb(143, 198, 248);border-color: rgb(143, 198, 248)"> Voir CV</button>
     
     <br><br><br>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal<?= $row['utilisateurID'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <!--zone d'ombre autour de la fenetre-->
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-body"><img id="image" src="./assets/cv/cv_emma_boul.png" alt="Click on button" height="700" width="600" /></div>
+                <div class="modal-body"><img id="image" src="<?= $row['CV'] ?>" alt="Click on button" height="700" width="600" /></div>
                 <div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: rgb(143, 198, 248);border-color: rgb(143, 198, 248)">Fermer</button></div>
             </div>
         </div>
